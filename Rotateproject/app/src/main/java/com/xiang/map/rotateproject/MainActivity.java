0dd943b6b6a4d0fe0045facd9375bbf0b329fc92
77377xiang <xiang792178903@sina.com>
2016-08-05 01:27:21 +0800

    中心旋转动画 实现旋转门一样翻页动画

---
+++ Rotateproject/app/src/main/java/com/xiang/map/rotateproject/MainActivity.java
@@ -0,0 +1,128 @@
+package com.xiang.map.rotateproject;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.view.animation.AccelerateInterpolator;
+        import android.view.animation.Animation;
+        import android.view.animation.Animation.AnimationListener;
+        import android.widget.Button;
+        import android.widget.ImageView;
+        import android.widget.RelativeLayout;
+        import android.app.Activity;
+/**
+ * Demo描述:
+ * 利用Camera实现图片的旋转动画
+ *
+ * 注意事项:
+ * 1 Rotate3dAnimation在APIDemo中的位置Views/Animation/3D Transition
+ * 2 待有空时候再仔细研究Rotate3dAnimation
+ *
+ * 参考资料:
+ * 1 http://blog.csdn.net/guolin_blog/article/details/10766017
+ * 2 http://blog.csdn.net/mapdigit/article/details/7804654
+ * 3 http://wang-peng1.iteye.com/blog/572886
+ *   Thank you very much
+ */
+public class MainActivity extends Activity {
+    private RelativeLayout mRelativeLayout;
+    private ImageView mImageView;
+    private Button mButton;
+    private int count=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        mRelativeLayout=(RelativeLayout) findViewById(R.id.relativeLayout);
+        mImageView=(ImageView) findViewById(R.id.imageView);
+        mButton=(Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float centerX = mRelativeLayout.getWidth() / 2f;
+                float centerY = mRelativeLayout.getHeight() / 2f;
+                // 构建3D旋转动画对象,旋转角度为0到90度
+                Rotate3dAnimation rotation = new Rotate3dAnimation(0, 90, centerX, centerY,310.0f, true);
+                // 动画持续时间500毫秒
+                rotation.setDuration(500);
+                // 动画完成后保持完成状态
+                rotation.setFillAfter(true);
+                rotation.setInterpolator(new AccelerateInterpolator());
+                // 设置动画的监听器
+                if (count%2==0) {
+                    rotation.setAnimationListener(new RotateToTheSecondImageViewAnimationListener());
+                }else{
+                    rotation.setAnimationListener(new RotateToTheFirstImageViewAnimationListener());
+                }
+                mRelativeLayout.startAnimation(rotation);
+                count++;
+            }
+        });
+    }
+
+
+    class RotateToTheFirstImageViewAnimationListener implements AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            mImageView.setImageResource(R.drawable.men);
+
+            // 获取布局的中心点位置，作为旋转的中心点
+            float centerX = mRelativeLayout.getWidth() / 2f;
+            float centerY = mRelativeLayout.getHeight() / 2f;
+
+            // 构建3D旋转动画对象,旋转角度为90到0度
+            Rotate3dAnimation rotation = new Rotate3dAnimation(90, 0, centerX, centerY,310.0f, false);
+            // 动画持续时间500毫秒
+            rotation.setDuration(500);
+            // 动画完成后保持完成状态
+            rotation.setFillAfter(true);
+            rotation.setInterpolator(new AccelerateInterpolator());
+            mRelativeLayout.startAnimation(rotation);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+
+    }
+
+
+    class RotateToTheSecondImageViewAnimationListener implements AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+        }
+
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            //动画完成后展示另外的图片
+            mImageView.setImageResource(R.drawable.c);
+
+            // 获取布局的中心点位置,作为旋转的中心点
+            float centerX = mRelativeLayout.getWidth() / 2f;
+            float centerY = mRelativeLayout.getHeight() / 2f;
+            // 构建3D旋转动画对象,旋转角度为270到360度
+            Rotate3dAnimation rotation = new Rotate3dAnimation(270, 360, centerX, centerY,310.0f, false);
+            // 动画持续时间500毫秒
+            rotation.setDuration(500);
+            // 动画完成后保持完成状态
+            rotation.setFillAfter(true);
+            rotation.setInterpolator(new AccelerateInterpolator());
+            mRelativeLayout.startAnimation(rotation);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+
+    }
+
+}
